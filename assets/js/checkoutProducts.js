ed6b8865380b4004d09df6fa2c5de133e36c887e
Allan Espitia <espitiaallan@gmail.com>
2023-08-30 23:26:07 -0600

    se agregaron funciones al admin

---
+++ assets/js/checkoutProducts.js
@@ -0,0 +1,33 @@
+
+const storedData = JSON.parse(localStorage.getItem("products"));
+const storedId = localStorage.getItem("product");//"1"
+
+// const arreglo = ["a","b","c"], arreglo [2]
+//parseInt("1") => 1
+
+const productoUno = storedData [parseInt(storedId)];//storedData[1]
+
+const name = document.getElementById("nom-producto");
+const price = document.getElementById("price");
+const size = document.getElementById("size");
+const stock = document.getElementById("stock");
+const description = document.getElementById("descripcion");
+const hide = document.getElementById("disguise");
+const photo = document.getElementById("photo");
+
+name.setAttribute("value", productoUno.name);
+price.setAttribute("value", productoUno.price);
+size.setAttribute("value", productoUno.size);
+stock.setAttribute("value", productoUno.stock);
+description.setAttribute("value", productoUno.description);
+description.innerHTML = productoUno.description;
+photo.setAttribute("src",productoUno.photo);
+hide.setAttribute("value",productoUno.hide);
+
+
+
+
+
+
+
+
--- assets/js/products.js
+++ assets/js/products.js
@@ -1,8 +1,11 @@
 class Product{
-    constructor(name, price, photo, description){
+    constructor(name, price, size, stock, hide, photo, description){
         this.id = this.calculateProductID();
         this.name = name;
         this.price = price;
+        this.size = size;
+        this.stock = stock;
+        this.hide = hide;
         this.photo = photo;
         this.description = description;
     };
@@ -43,22 +46,22 @@ function downloadDataLocalStorage(){
 };
 
 function displayCards(products){
-    //Creating cards from array of objects
-    let productCards = products.map( product => 
-        `<a href="#" class="grid-product-item">
-            <img src="${product.photo}" class="card-img-top" alt="Bebida Crazy Ichigo(Maracuya)">
-            <div class="card-body">
-            <p class="card-description text-center" > ${product.name} </p>
-            <p class="card-description text-center"> ${product.price} </p>
-            <p class="card-text">${product.description}</p>
-            </div>
-        </a>`
-        );
-    //Getting product container to display products
-    const productosContainer = document.getElementById("grid-products-container");
+    // //Creating cards from array of objects
+    // let productCards = products.map( product => 
+    //     `<a href="#" class="grid-product-item">
+    //         <img src="${product.photo}" class="card-img-top" alt="Bebida Crazy Ichigo(Maracuya)">
+    //         <div class="card-body">
+    //         <p class="card-description text-center" > ${product.name} </p>
+    //         <p class="card-description text-center"> ${product.price} </p>
+    //         <p class="card-text">${product.description}</p>
+    //         </div>
+    //     </a>`
+    //     );
+    // //Getting product container to display products
+    // const productosContainer = document.getElementById("grid-products-container");
 
-    //Display array of products in DOM
-    productosContainer.innerHTML = productCards.join("");
+    // //Display array of products in DOM
+    // productosContainer.innerHTML = productCards.join("");
             
 };
 
@@ -66,35 +69,120 @@ function createProducts(){
     const products = [];
     let product;
    
-    product = new Product("Caramelo","$80","/assets/img/carmelo.webp","Dulce bebida con perlas de tapioca en caramelo suave y delicioso.");
+    product = new Product("Caramelo","$80","grande",50,1,"/assets/img/carmelo.webp","Dulce bebida con perlas de tapioca en caramelo suave y delicioso.");
     product.loadDataLocalStorage();
 
-    product = new Product("Explosión de chocolate","$70","/assets/img/chocolate.webp","Sedosa bebida con perlas de tapioca sumergidas en un cremoso y rico chocolate.")
+    product = new Product("Explosión de chocolate", "$70", "grande", 50, 1,"/assets/img/chocolate.webp","Sedosa bebida con perlas de tapioca sumergidas en un cremoso y rico chocolate.")
     product.loadDataLocalStorage();
 
-    product = new Product("Fresa Refresher","$60","/assets/img/fresaRefresher.webp","Refrescante bebida con perlas de tapioca en aroma y sabor a fresa jugosa.")
+    product = new Product("Fresa Refresher", "$60", "grande", 50, 1, "/assets/img/fresaRefresher.webp","Refrescante bebida con perlas de tapioca en aroma y sabor a fresa jugosa.")
     product.loadDataLocalStorage();
 
-    product = new Product("Granada","$50","/assets/img/granada.webp","Vibrante bebida con perlas de tapioca y el dulzor único de la granada.")
+    product = new Product("Granada", "$50", "grande", 50, 1, "/assets/img/granada.webp","Vibrante bebida con perlas de tapioca y el dulzor único de la granada.")
     product.loadDataLocalStorage();
 
-    product = new Product("Ken","$90","/assets/img/ken.webp","Refrescante bebida con perlas de tapioca en sabor a uva jugosa y dulce.");
+    product = new Product("Ken", "$90", "grande", 50, 1, "/assets/img/ken.webp","Refrescante bebida con perlas de tapioca en sabor a uva jugosa y dulce.");
     product.loadDataLocalStorage();
 
-    product = new Product("Mora Azul","$70","/assets/img/moraazul.webp","Deliciosa bebida con perlas de tapioca y el sabor agridulce de las moras azules.");
+    product = new Product("Mora Azul", "$70", "grande", 50, 1, "/assets/img/moraazul.webp","Deliciosa bebida con perlas de tapioca y el sabor agridulce de las moras azules.");
     product.loadDataLocalStorage();
 
-    product = new Product("Kiwi Refresher","$80","/assets/img/kiwirefresher.webp","Refrescante bebida con perlas de tapioca en sabor a kiwi tropical y vivaz.");
+    product = new Product("Kiwi Refresher", "$80", "grande", 50, 1, "/assets/img/kiwirefresher.webp","Refrescante bebida con perlas de tapioca en sabor a kiwi tropical y vivaz.");
     product.loadDataLocalStorage();
 
-    product = new Product("Maracuyá","$60","/assets/img/maracuya.webp","Exótica bebida con perlas de tapioca y el sabor audaz del maracuyá.");
+    product = new Product("Maracuyá", "$60", "grande", 50, 1, "/assets/img/maracuya.webp","Exótica bebida con perlas de tapioca y el sabor audaz del maracuyá.");
     product.loadDataLocalStorage();
 
-    product = new Product("Maracuyá","$60","/assets/img/maracuya.webp","Exótica bebida con perlas de tapioca y el sabor audaz del maracuyá.");
+    product = new Product("Maracuyá", "$60", "grande", 50, 1, "/assets/img/maracuya.webp","Exótica bebida con perlas de tapioca y el sabor audaz del maracuyá.");
     product.loadDataLocalStorage();
     
-    product = new Product("Vanilla","$65","/assets/img/vanilla.webp"," Suave bebida con perlas de tapioca y el delicado aroma de la vainilla.");
+    product = new Product("Vanilla", "$65", "grande", 50, 1, "/assets/img/vanilla.webp"," Suave bebida con perlas de tapioca y el delicado aroma de la vainilla.");
     product.loadDataLocalStorage();    
 }
 
 downloadDataLocalStorage();
+
+
+
+// Botón Ver producto 
+
+
+const viewProduct = document.getElementById("checkoutProduct");
+viewProduct.addEventListener (`click`, ()=> { 
+    // if (localSotrage.getItem("prodcuts") ) { 
+    //     const storedData = JSON.parse( localSotrage.getItem("products"));
+    // };
+    
+    const keyProduct = "1";
+    localStorage.setItem("product", keyProduct );
+
+    
+
+    
+
+}); 
+
+
+
+
+
+
+
+
+
+
+
+
+//---------modificar producto----------
+
+const editProduct = document.getElementById("editProduct")
+editProduct.addEventListener('click', ()=>{
+    
+    console.log('Hola');
+    
+   /*  const idProducto = document.getElementsByClassName("id-product");
+    idContenido = idProducto.innerHTML;
+    console.log(idContenido);
+
+
+    localStorage
+ */
+
+
+
+});
+
+
+
+
+
+
+
+// Eliminar producto
+const deleteProduct = document.getElementById("deleteProduct");
+deleteProduct.addEventListener('click', ()=>{
+    /*  -- Tomar el contenido de la etiqueta id  -- */
+    /* nos lleva a la etiqueta padre: cardProductExample */
+    const cardProductExample = deleteProduct.parentNode.parentNode;
+
+    /* ahora nos vamos una etiqueta hija para tomar el valor de ID*/
+    const idProduct = parseInt(cardProductExample.querySelector('.id-product').innerHTML) 
+
+    /* ahora buscamos este id en el localStorage y lo eliminamos*/
+    let storedData = JSON.parse(localStorage.getItem("products"));
+    
+    console.log(storedData)
+    storedData = storedData.filter( product => {
+        return product.id !== idProduct
+    } )
+    console.log(storedData)
+
+    /* guardamos los datos actualizados al local storage */
+    localStorage.setItem("products",JSON.stringify(storedData));
+
+    
+    /* refrescar pagina al terminar de eliminar el producto */
+    /* setTimeout(function() {
+        location.reload();
+    }, 2000); */
+})
