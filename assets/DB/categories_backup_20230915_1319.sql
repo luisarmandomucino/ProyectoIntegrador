3d507ee274d0253dd416062eafd462670405f06c
luis muciño <nievesmucino@gmail.com>
2023-09-15 13:40:00 -0600

    Comenzando database de bubble bear

---
+++ assets/DB/categories_backup_20230915_1319.sql
@@ -0,0 +1,18 @@
+SELECT * FROM categories;
+USE bbdatabase; 
+
+INSERT 
+INTO categories ( sale, outstanding)
+VALUES (  1, 1);
+INSERT 
+INTO categories ( sale, outstanding)
+VALUES (  0, 1);
+INSERT 
+INTO categories ( sale, outstanding)
+VALUES ( 1, 0);
+INSERT 
+INTO categories ( sale, outstanding)
+VALUES (0, 0);
+
+
+
